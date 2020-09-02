c1f05f2ebeabeb488601a374c993588f02ad1459
JamesMc <jamesjohnmcguire@gmail.com>
2020-09-02 23:09:13 +0900

    New bug test project.

---
+++ ImageTest/ImageTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ImageTest.ViewModels;
+
+namespace ImageTest.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
